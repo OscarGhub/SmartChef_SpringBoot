2fc5f0452686601f06f958bf938d7b7dbc487fa1
Oscar <ogarciadelgado@safareyes.es>
2026-01-29 12:22:43 +0100

    cambio usuario a usuarios

--- src/main/java/com/oscar/proyecto/controladores/UsuarioController.java
+++ src/main/java/com/oscar/proyecto/controladores/UsuarioController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/usuario")
+@RequestMapping("/api/usuarios")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
